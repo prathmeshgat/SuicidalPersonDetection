52c0e821b9e4fa4f2cba487403fa5df7518c19fe
prathmeshgat <gat@usc.edu>
2016-07-23 23:06:11 -0700

    added comment repo

---
+++ DataAccess/Models/Comment.py
@@ -0,0 +1,43 @@
+__author__ = 'Prathmesh'
+
+class Comment:
+
+    def __init__(self, _id,text,category,nnFraction=0,vbFration=0,advFraction=0,prp1Fraction=0,prp2Fraction=0,
+                 cleanedToken=list(),posSentiment=0,negSentiment=0,neuSentiment=0,compoundSentiment=0,
+                 custom1=None,custom2 =None,custom3 = None,custom4 =None,custom5 = None):
+
+        self._id = _id
+
+        self.text = text
+
+        self.category = category
+
+        self.nnFraction = nnFraction
+
+        self.vbFration = vbFration
+
+        self.advFraction = advFraction
+
+        self.prp1Fraction = prp1Fraction
+
+        self.prp2Fraction = prp2Fraction
+
+        self.cleanedToken = cleanedToken
+
+        self.posSentiment = posSentiment
+
+        self.negSentiment = negSentiment
+
+        self.neuSentiment = neuSentiment
+
+        self.compoundSentiment = compoundSentiment
+
+        self.custom1 = custom1
+
+        self.custom2 = custom2
+
+        self.custom3 = custom3
+
+        self.custom4 = custom4
+
+        self.custom5 = custom5
---
+++ DataAccess/Repos/CommentRepo.py
@@ -0,0 +1,68 @@
+__author__ = 'Prathmesh'
+
+import json
+import DataAccess.Models.Comment as Model
+from pymongo import MongoClient
+
+class CommentRepo:
+    def __init__(self):
+
+        self.client = MongoClient('localhost', 27017)
+
+        self.db = self.client.coreData
+
+        self.collection = self.db.CommentsSet
+
+    def insert(self,comment):
+        result = self.collection.insert(comment.__dict__)
+        return result
+
+    def object_decoder(self,obj):
+        return Model.Comment(obj['_id'],obj['text'],obj['category'],obj['nnFraction'],obj['vbFration'],
+                            obj['advFraction'],obj['prp1Fraction'],obj['prp2Fraction'],
+                            obj['cleanedToken'],obj['posSentiment'],obj['negSentiment'],obj['neuSentiment'],
+                            obj['compoundSentiment'],obj['custom1'],obj['custom2'],
+                            obj['custom3'],obj['custom4'],obj['custom5'])
+
+    def get(self,Id):
+        comment = self.collection.find_one({"_id": Id})
+
+        return self.object_decoder(comment)
+
+    def getSuicidalCommentSet(self):
+        commentSet = self.collection.find({"category": "S"})
+        commentList = list()
+        for comment in commentSet:
+            commentList.append(self.object_decoder(comment))
+        return commentList
+
+    def getPersonalNarrationCommentSet(self):
+        commentSet = self.collection.find({"category": "PN"})
+        commentList = list()
+        for comment in commentSet:
+            commentList.append(self.object_decoder(comment))
+        return commentList
+
+    def delete(self,Id):
+       result = self.collection.delete_many({"_id": Id})
+       return result.deleted_count
+
+    def cleanCollection(self):
+       self.collection.drop()
+
+    def getAll(self):
+        commentSet = self.collection.find()
+        commentList = list()
+        for comment in commentSet:
+            commentList.append(self.object_decoder(comment))
+        return commentList
+
+    def update(self,comment):
+        id =comment._id
+        temp =comment.__dict__
+        _dict = {key: value for key, value in temp.items() if value is not "_id"}
+        result = self.collection.replace_one(
+            {"_id": id},
+            _dict
+        )
+        return result.matched_count
